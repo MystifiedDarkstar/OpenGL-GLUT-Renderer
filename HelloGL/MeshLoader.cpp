d57c7eb3c463e872887f55fbda06126844814f9e
Aidan <118263298+MystifiedDarkstar@users.noreply.github.com>
2024-06-03 13:52:52 +0100

    Update MeshLoader.cpp

--- HelloGL/MeshLoader.cpp
+++ HelloGL/MeshLoader.cpp
@@ -7,69 +7,10 @@ using namespace std;
 
 namespace MeshLoader
 {
-	//void LoadVertices(ifstream& inFile, Mesh& mesh);
-	//void LoadNormals(ifstream& inFile, Mesh& mesh);
-	//void LoadTexCoords(ifstream& inFile, TexturedMesh& mesh);
-	//void LoadIndices(ifstream& inFile, Mesh& mesh);
-
 	void LoadVerticesOBJ(Mesh& mesh);
 	void LoadTexCoordsOBJ(Mesh& mesh);
 	void LoadNormalsOBJ(Mesh& mesh);
 	void LoadIndicesOBJNew(Mesh& mesh);
-	void LoadIndicesOBJ(Mesh& mesh);
-
-	//void LoadVertices(ifstream& inFile, Mesh& mesh)
-	//{
-	//	inFile >> mesh.VertexCount;
-
-	//	if (mesh.VertexCount > 0)
-	//	{
-	//		mesh.Vertices = new Vertex[mesh.VertexCount];
-
-	//		for (int i = 0; i < mesh.VertexCount; i++)
-	//		{
-	//			inFile >> mesh.Vertices[i].x;
-	//			inFile >> mesh.Vertices[i].y;
-	//			inFile >> mesh.Vertices[i].z;
-	//		}
-	//	}
-	//}
-
-	//void LoadNormals(ifstream& inFile, Mesh& mesh)
-	//{
-	//	//TODO: LOAD COLOURS
-	//	inFile >> mesh.NormalCount;
-	//	mesh.Normals = new Vector3[mesh.NormalCount];
-	//	for (int i = 0; i < mesh.NormalCount; i++)
-
-	//	{
-	//		inFile >> mesh.Normals[i].x;
-	//		inFile >> mesh.Normals[i].y;
-	//		inFile >> mesh.Normals[i].z;
-	//	}
-	//}
-
-	//void LoadTexCoords(ifstream& inFile, Mesh& mesh)
-	//{
-	//	//TODO: Load TexCoords
-	//	inFile >> mesh.Mesh->TexCoordCount;
-	//	mesh.Mesh->TexCoords = new TexCoord[mesh.Mesh->TexCoordCount];
-	//	for (int i = 0; i < mesh.Mesh->TexCoordCount; i++)
-	//	{
-	//		inFile >> mesh.Mesh->TexCoords[i].U;
-	//		inFile >> mesh.Mesh->TexCoords[i].V;
-	//	}
-	//}
-	//void LoadIndices(ifstream& inFile, Mesh& mesh)
-	//{
-	//	//TODO: Load Indices
-	//	inFile >> mesh.VertexIndexCount;
-	//	mesh.VertexIndices = new GLushort[mesh.VertexIndexCount];
-	//	for (int i = 0; i < mesh.VertexIndexCount; i++)
-	//	{
-	//		inFile >> mesh.VertexIndices[i];
-	//	}
-	//}
 
 	void LoadIndicesOBJNew(Mesh& mesh)
 	{
@@ -89,15 +30,6 @@ namespace MeshLoader
 		{
 			mesh.NormalsIndices[i] = *mesh.NormalIndicesVector[i];
 		}
-	}
-	void LoadIndicesOBJ(Mesh& mesh)
-	{
-		mesh.VertexIndices = new GLushort[mesh.VertexIndexCount];
-		for (int i = 0; i < mesh.VertexIndexCount; i++)
-		{
-			mesh.VertexIndices[i] = *mesh.VertexIndicesVector[i];
-		}
-
 	}
 
 	void LoadVerticesOBJ(Mesh& mesh)
@@ -126,54 +58,6 @@ namespace MeshLoader
 			mesh.Normals[i] = *mesh.NormalVector[i];
 		}
 	}
-
-	//Mesh* MeshLoader::Load(char* path)
-	//{
-	//	Mesh* mesh = new Mesh();
-
-	//	ifstream inFile;
-
-	//	inFile.open(path);
-
-	//	if (!inFile.good())  
-	//	{
-	//		cerr  << "Can't open texture file " << path << endl;
-	//		return nullptr;
-	//	}
-
-	//	//LOAD DATA USING METHODS ABOVE
-	//	LoadVertices(inFile , *mesh);
-	//	LoadNormals(inFile , *mesh);
-	//	LoadIndices(inFile, *mesh);
-
-
-	//	return mesh;
-	//}
-
-	//TexturedMesh* MeshLoader::LoadTextured(char* path)
-	//{
-	//	TexturedMesh* mesh = new TexturedMesh();
-	//	mesh->Mesh = new Mesh();
-
-	//	//LOAD FILE AND DATA
-	//	ifstream inFile;
-
-	//	inFile.open(path);
-
-	//	if (!inFile.good())
-	//	{
-	//		cerr << "Can't open texture file " << path << endl;
-	//		return nullptr;
-	//	}
-
-	//	//LOAD DATA USING METHODS ABOVE
-	//	LoadVertices(inFile, *mesh->Mesh);
-	//	LoadTexCoords(inFile, *mesh);
-	//	LoadNormals(inFile, *mesh->Mesh);
-	//	LoadIndices(inFile, *mesh->Mesh);
-
-	//	return mesh;
-	//}
 
 	Mesh* MeshLoader::LoadOBJNew(char* path)
 	{
@@ -261,60 +145,5 @@ namespace MeshLoader
 		return mesh;
 	}
 }
-//	Mesh* MeshLoader::LoadOBJ(char* path)
-//	{
-//		Mesh* mesh = new Mesh();
-//
-//		//LOAD FILE AND DATA
-//		ifstream inFile;
-//
-//		inFile.open(path);
-//
-//		if (!inFile.good())
-//		{
-//			cerr << "Can't open file " << path << endl;
-//			return nullptr;
-//		}
-//
-//		char firstLineChar;
-//		while (inFile >> firstLineChar)
-//		{
-//			if (firstLineChar == 'v')
-//			{
-//				Vertex* temp = new Vertex();
-//				inFile >> temp->x;
-//				inFile >> temp->y;
-//				inFile >> temp->z;
-//				mesh->VertexVector.push_back(temp);
-//				mesh->VertexCount++;
-//			}
-//			else if (firstLineChar == 'f')
-//			{
-//				for (int i = 0; i < 3; i++)
-//				{
-//
-//					GLushort* vertexIndexTemp = new GLushort;
-//
-//					inFile >> *vertexIndexTemp;
-//
-//					*vertexIndexTemp -= 1;
-//
-//					mesh->VertexIndicesVector.push_back(vertexIndexTemp);
-//					mesh->VertexIndexCount++;
-//				}
-//			}
-//			else
-//			{
-//				std::cout << "Letter Not Recognised! Char: " << firstLineChar << std::endl;
-//			}
-//		}
-//
-//		LoadVerticesOBJ(*mesh);
-//		LoadIndicesOBJ(*mesh);
-//
-//		inFile.close();
-//
-//		return mesh;
-//	}
-//}
+
 
